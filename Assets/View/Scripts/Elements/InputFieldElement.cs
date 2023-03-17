e6776b523664daa69ad402137d05bf044297153d
Changbai Guo <33477119+CBVeLuMe@users.noreply.github.com>
2023-03-16 20:16:39 -0400

    增加 Input Field Prefab

---
+++ Assets/View/Scripts/Elements/InputFieldElement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class InputFieldElement : MonoBehaviour
+{
+    [SerializeField]
+    TMP_InputField inputField;
+    [SerializeField]
+    Animator animator;
+    string onSelectState = "Activated";
+    string onEndEditState = "Inactive";
+
+    private void Start()
+    {
+        if (inputField == null)
+            inputField = gameObject.GetComponent<TMP_InputField>();
+        if (animator == null)
+            animator = gameObject.GetComponent<Animator>();
+        if (inputField.onSelect.GetPersistentEventCount() == 0)
+            inputField.onSelect.AddListener(delegate { AnimatorOnSelect(); });
+        if (inputField.onEndEdit.GetPersistentEventCount() == 0)
+            inputField.onEndEdit.AddListener(delegate { AnimatorOnEndEdit(); });
+    }
+
+    public void AnimatorOnSelect()
+    {
+        animator.Play(onSelectState);
+    }
+
+    public void AnimatorOnEndEdit()
+    {
+        if (inputField.text.Length == 0)
+            animator.Play(onEndEditState);
+    }
+
+    private void UpdateAnimator()
+    {
+        if (inputField.text.Length == 0)
+            AnimatorOnEndEdit();
+        else
+            AnimatorOnSelect();
+    }
+}
